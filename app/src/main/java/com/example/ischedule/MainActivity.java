da2d2b6a89729377e72b7372673a72afd4abc308
Miki <miguelsanblassanchez@gmail.com>
2019-05-29 23:37:23 +0200

    Rutina en el menu para saber si tenemos datos o no en el dispositivo con Broadcast receivers

--- app/src/main/java/com/example/ischedule/MainActivity.java
+++ app/src/main/java/com/example/ischedule/MainActivity.java
@@ -14,6 +14,8 @@ import android.database.sqlite.SQLiteException;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,6 +26,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.util.Log;
 import android.view.ActionMode;
 import android.view.View;
 import android.view.Menu;
@@ -50,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     private NotificationManager mNotifyManager;
     private NotificationReceiver mReceiver = new NotificationReceiver();
     private String evento = "";
+    private String servicioDatos = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
             sendNotification(evento);
         }
 
+        //MODIFICAR ESTO
         //sendNotification("IR AL CINE");
 
     }
@@ -153,6 +158,9 @@ public class MainActivity extends AppCompatActivity {
         }else if(id == R.id.action_nofav){
             favoritosNo(item);
             return true;
+        }else if(id == R.id.action_info){
+          //  servicioDatos = "Datos Conectados!";
+            Toast.makeText(getApplicationContext(), servicioDatos, Toast.LENGTH_LONG).show();
         }
 
         return super.onOptionsItemSelected(item);
@@ -167,7 +175,6 @@ public class MainActivity extends AppCompatActivity {
         Datos_Mask datos = null;
         Cursor cursor = db.rawQuery(variables_globales.Consul_select+variables_globales.tabla+" WHERE "+variables_globales.campo_favorito+"=0", null);
         while(cursor.moveToNext()){
-            System.out.println("ENTRO EN ESTE BUCLE");
             datos = new Datos_Mask(cursor.getString(1),cursor.getString(2),(int) enviarDatos(cursor.getString(6)), cursor.getInt(0));
             listaDatos.add(datos);
         }
@@ -193,8 +200,6 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = conn.getReadableDatabase();
         Datos_Mask datos = null;
         Cursor cursor = db.rawQuery(variables_globales.Consul_select+variables_globales.tabla, null);
-        //Cursor cursor = db.rawQuery(variables_globales.Consul_select+variables_globales.tabla+" WHERE "+variables_globales.campo_favorito+"=0", null);
-        //  Cursor cursor = db.query(variables_globales.tabla, campos, variables_globales.campo_id+"=?", parametro, null, null, null);
         while(cursor.moveToNext()){
             System.out.println("ENTRO EN ESTE BUCLE");
             datos = new Datos_Mask(cursor.getString(1),cursor.getString(2),(int) enviarDatos(cursor.getString(6)), cursor.getInt(0));
@@ -224,30 +229,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createNotificationChannel() {
-        mNotifyManager =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        if (android.os.Build.VERSION.SDK_INT >=
-                android.os.Build.VERSION_CODES.O) {
-
-            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
-                    "Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
-
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,"Mascot Notification", NotificationManager.IMPORTANCE_HIGH);
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(true);
-            notificationChannel.setDescription
-                    (getString(R.string.notification_channel_description));
-
+            notificationChannel.setDescription(getString(R.string.notification_channel_description));
             mNotifyManager.createNotificationChannel(notificationChannel);
         }
     }
 
     private NotificationCompat.Builder getNotificationBuilder(String evento) {
         Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent notificationPendingIntent = PendingIntent.getActivity
-                (this, NOTIFICATION_ID, notificationIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder notifyBuilder = new NotificationCompat
                 .Builder(this, PRIMARY_CHANNEL_ID)
@@ -262,26 +258,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendNotification(String titulo) {
         Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
-        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this,
-                NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
-
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder(titulo);
-
-//        notifyBuilder.addAction(R.drawable.ic_update,
-//                getString(R.string.update), updatePendingIntent);
-
         mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
-
-     //   setNotificationButtonState(false, true, true);
 
     }
 
-//    public void cancelNotification() {
-//        mNotifyManager.cancel(NOTIFICATION_ID);
-//
-//      //  setNotificationButtonState(true, false, false);
-//
-//    }
+
+
 
     public class NotificationReceiver extends BroadcastReceiver {
 
@@ -296,7 +280,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //////////////////////////////////////////////////
+    /////////////BROADCAST//////////////////////////
+    ////////////////////////////////////////////////
 
+    private BroadcastReceiver networkStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo info = manager.getActiveNetworkInfo();
+            verificarConexion(info);
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(networkStateReceiver, new IntentFilter(android.net.ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    public void onPause() {
+        unregisterReceiver(networkStateReceiver);
+        super.onPause();
+    }
+
+    private void verificarConexion(NetworkInfo networkInfo) {
+        if (networkInfo != null) {
+            if (networkInfo.isConnected()) {
+                servicioDatos = "Datos Conectados!";
+              //  Toast.makeText(getApplicationContext(), "Datos Conectados!", Toast.LENGTH_LONG).show();
+              //  Log.d("MenuActivity", "CONECTADO");
+            }
+        }else{
+            servicioDatos = "Datos Desconectados!";
+           // Toast.makeText(getApplicationContext(), "Datos Desconectados!", Toast.LENGTH_LONG).show();
+          //  Log.d("MenuActivity", "DESCONECTADO");
+        }
+    }
 
 
 
